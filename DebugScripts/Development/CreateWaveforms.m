77bb5a0260d2386a26145021b49c569c68b367f9
Joe Heffernan <heffjos@umich.edu>
2017-01-10 10:34:11 -0600

    Create signals for 2 runs now. Signal characteristics are more random.

--- DebugScripts/Development/CreateWaveforms.m
+++ DebugScripts/Development/CreateWaveforms.m
@@ -13,8 +13,6 @@ WaitFrames = round(FlipSecs / Refresh); % display signal every this frame
 MaxX = Scale*120;
 N = Scale*4*1/FlipSecs + Scale*10*1/FlipSecs-1;
 
-Sin1 = 5.5*sin(2*pi*(1:N)*(1/60));
-Sin2 = 1*sin(2*pi*(1:N)*(1/2));
 
 % 4 initial baseline + 1 baseline + 5 ramp + 4 max
 Index1 = 1:(5*Scale*1/FlipSecs-1);
@@ -24,40 +22,64 @@ Index3 = (Index2(end)+1):N;
 Inc = 85/length(Index2);
 Ramp = 0:Inc:(85-Inc);
 
+NumRuns = 2
 % Baselines
 % Signals
-for i = 1:15
-    Noise = 5*randn(1, N);
-    T1 = randi([0 120]);
-    T2 = randi([30 240]);
-    if mod(T2, 2)
-        T2 = T2 - 1;
+for iRun = 1:NumRuns
+    for i = 1:15
+        Noise = 5*randn(1, N);
+        T1 = randi([60 180]);
+        T2 = randi([120 240]);
+        if mod(T2, 2)
+            T2 = T2 - 1;
+        end
+    
+        Index1 = 1:(4*Scale*1/FlipSecs + T1);
+        Index2 = (Index1(end)+1):(Index1(end)+1+T2-1);
+        Index3 = (Index2(end)+1):N;
+        Inc = 85/length(Index2);
+        Ramp = 0:Inc:(85-Inc);
+    
+        Phase1 = 2*rand(1, 1);
+        Phase2 = 2*rand(1, 1);
+    
+        Mag1 = 3 + (6-3)*rand(1, 1);
+        Mag2 = 0.5 + (2-0.5)*rand(1, 1);
+        fprintf(1, 'Mag1:%0.2f, Mag1:%0.2f\n', Mag1, Mag2);
+        
+        Sin1 = Mag1*sin(2*pi*(1:N)*(1/60) + Phase1*pi);
+        Sin2 = Mag2*sin(2*pi*(1:N)*(1/2) + Phase2*pi);
+    
+        if i < 9
+            Baselines{iRun}{i, 1} = Noise(Index1);
+            Baselines{iRun}{i, 2} = Noise(Index2);
+            Baselines{iRun}{i, 3} = Noise(Index3);
+        end
+    
+        Signals{iRun}{i, 1} = Noise(Index1);
+        Signals{iRun}{i, 2} = Noise(Index2) + Ramp + Sin1(Index2) + Sin2(Index2);
+        Signals{iRun}{i, 2}(Signals{iRun}{i, 2} > 100) = 99;
+        Signals{iRun}{i, 3} = Noise(Index3) + 85 + Sin1(Index3) + Sin2(Index3);
+        Signals{iRun}{i, 3}(Signals{iRun}{i, 3} > 100) = 99;
     end
-
-    Index1 = 1:(4*Scale*1/FlipSecs + T1);
-    Index2 = (Index1(end)+1):(Index1(end)+1+T2-1);
-    Index3 = (Index2(end)+1):N;
-    Inc = 85/length(Index2);
-    Ramp = 0:Inc:(85-Inc);
-
-    if i < 9
-        Baselines{i, 1} = Noise(Index1);
-        Baselines{i, 2} = Noise(Index2);
-        Baselines{i, 3} = Noise(Index3);
-    end
-
-    Signals{i, 1} = Noise(Index1);
-    Signals{i, 2} = Noise(Index2) + Ramp + Sin1(Index2) + Sin2(Index2);
-    Signals{i, 2}(Signals{i, 2} > 100) = 99;
-    Signals{i, 3} = Noise(Index3) + 85 + Sin1(Index3) + Sin2(Index3);
-    Signals{i, 3}(Signals{i, 3} > 100) = 99;
 end
 
-for i = 1:size(Signals, 1)
-    for k = 1:size(Signals, 2)
-        fprintf(1, '%d:%0.3f ', k, max(Signals{i, k}));
+for iRun = 1:NumRuns
+    for i = 1:size(Signals{iRun}, 1)
+        for k = 1:size(Signals{iRun}, 2)
+            fprintf(1, '%d:%d:%0.3f ', iRun, k, max(Signals{iRun}{i, k}));
+        end
+        fprintf(1, '\n');
     end
-    fprintf(1, '\n');
+end
+
+for iRun = 1:NumRuns
+    for i = 1:size(Signals{iRun}, 1)
+        for k = 1:size(Signals{iRun}, 2)
+            fprintf(1, '%d:%d:%0.2f ', iRun, k, length(Signals{iRun}{i, k})/60);
+        end
+        fprintf(1, '\n');
+    end
 end
 
 save('Waveforms', 'Baselines', 'Signals');
