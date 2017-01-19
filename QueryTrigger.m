2afc6e2173be579fc3b90528b1f2f2c509df6ed8
Joe Heffernan <heffjos@umich.edu>
2017-01-18 23:38:42 -0600

    Added script QueryTrigger to show the trigger output.

---
+++ QueryTrigger.m
@@ -0,0 +1,112 @@
+clear all;
+PsychDebugWindowConfiguration
+
+try
+    KbReleaseWait;
+
+    % change preferences
+    Screen('Preference', 'SkipSyncTests', 2);
+    Screen('Preference', 'VisualDebugLevel', 3);
+
+    % screen initialization and refresh
+    Screens = Screen('Screens'); % get scren number
+    ScreenNumber = max(Screens);
+    [Window, Rect] = Screen('OpenWindow', ScreenNumber, [0 0 0]);
+    Screen('ColorRange', Window, 1, [], 1);
+    PriorityLevel = MaxPriority(Window);
+    Priority(PriorityLevel);
+    [XCenter, YCenter] = RectCenter(Rect);
+    [Refresh] = Screen('GetFlipInterval', Window);
+    
+    KbName('UnifyKeyNames');
+    KbNames = KbName('KeyNames');
+
+    % set up text properties
+    Screen('TextFont', Window, 'Arial');
+    Screen('TextSize', Window, 50);
+    Screen('TextColor', Window, [1 1 1]);
+
+    % create KbQueue
+    ListenChar(-1);
+    KbQueueCreate([]);
+
+    DrawFormattedText(Window, ['Waiting for scanner trigger\n\n' ...
+        'Do NOT press any other buttons on input computer'], 'center', 'center');
+    BeginTime = Screen('Flip', Window);
+
+    KbQueueStart([]);
+
+    while 1
+        [Pressed, TimeKeysPressed] = KbQueueCheck([]);
+        if Pressed
+            % get keys
+            TimeKeysPressed(TimeKeysPressed == 0) = nan;
+            [TimeKeysPressed, Reorder] = sort(TimeKeysPressed);
+            KeysPressed = KbNames(Reorder(~isnan(TimeKeysPressed)));
+            TimeKeysPressed(isnan(TimeKeysPressed)) = [];
+            for i = 1:numel(KeysPressed)
+                FirstKeySet{i, 1} = KeysPressed{i};
+                FirstKeySet{i, 2} = TimeKeysPressed(i) - BeginTime;
+            end
+            clear i
+            
+            DrawFormattedText(Window, ['Trigger found\n\n' ...
+                'Waiting 2 seconds. Do NOT press anything in the mean time.'], ...
+                'center', 'center');
+            Screen('Flip', Window);
+            WaitSecs(2);
+            KbQueueStop([]);
+            break;
+        end
+    end
+
+    [Pressed, TimeKeysPressed] = KbQueueCheck([]);
+    KbQueueRelease([]);
+    TimeKeysPressed(TimeKeysPressed == 0) = nan;
+    [TimeKeysPressed, Reorder] = sort(TimeKeysPressed);
+    KeysPressed = KbNames(Reorder(~isnan(TimeKeysPressed)));
+    TimeKeysPressed(isnan(TimeKeysPressed)) = [];
+    for i = 1:numel(KeysPressed)
+        SecondKeySet{i, 1} = KeysPressed{i};
+        SecondKeySet{i, 2} = TimeKeysPressed(i) - BeginTime;
+    end
+    clear i
+
+    OutDir = fullfile(pwd, 'QueryScanner');
+    mkdir(OutDir);
+    Outfile = fullfile(OutDir, 'QueryTrigger.txt');
+    Fid = fopen(Outfile, 'w');
+
+    fprintf(Fid, '*** First key set ***\n');
+    for i = 1:size(FirstKeySet, 1)
+        fprintf(Fid, '%s %0.4f\n', FirstKeySet{i, 1}, FirstKeySet{i, 2});
+    end
+    clear i
+
+    fprintf(Fid, '*** Second key set ***\n');
+    for i = 1:size(SecondKeySet, 1)
+        fprintf(Fid, '%s %0.4f\n', SecondKeySet{i, 1}, SecondKeySet{i, 2});
+    end
+    clear i
+    fclose(Fid);
+
+    sca;
+    ShowCursor;
+    ListenChar(0);
+    Priority(0);
+catch err
+    ShowCursor;
+    sca;
+    ListenChar(0);
+    Priority(0);
+
+    OutDir = fullfile(pwd, 'QueryScanner');
+    mkdir(OutDir);
+    Outfile = fullfile(OutDir, 'QueryTrigger.txt');
+    Fid = fopen(Outfile, 'w');
+    fprintf(Fid, 'Error: %s\n', err.message);
+    fclose(Fid);
+
+    fclose('all');
+    rethrow(err);
+end
