4d12188ef5448fea0e2650c96bcbcabaf69a418e
Joe Heffernan <heffjos@umich.edu>
2017-01-18 20:51:53 -0600

    Finished GetDevices

--- GetDevices.m
+++ GetDevices.m
@@ -1,30 +1,87 @@
 PsychDebugWindowConfiguration
-PsychDefaultSetup(2); % default settings
 
-% screen initialization and refresh
-Screens = Screen('Screens'); % get scren number
-ScreenNumber = max(Screens);
-[Window, Rect] = PsychImaging('OpenWindow', ScreenNumber);
-PriorityLevel = MaxPriority(Window);
-Priority(PriorityLevel);
-[XCenter, YCenter] = RectCenter(Rect);
-[Refresh] = Screen('GetFlipInterval', Window);
-ScanRect = [0 0 1024 768];
-[ScanCenter(1), ScanCenter(2)] = RectCenter(ScanRect);
+try
+    % screen initialization and refresh
+    Screens = Screen('Screens'); % get scren number
+    ScreenNumber = max(Screens);
+    [Window, Rect] = Screen('OpenWindow', ScreenNumber);
+    Info = Screen('GetWindowInfo', Window);
+    PriorityLevel = MaxPriority(Window);
+    Priority(PriorityLevel);
+    [XCenter, YCenter] = RectCenter(Rect);
+    [Refresh] = Screen('GetFlipInterval', Window);
+    OutIndex = 1;
 
-% print scren information
-fprintf(1, '\n*** GetDevices INFORMATION ***\n');
-fprintf(1, 'Screen refresh rate: %0.4f Hz\n', 1/Refresh);
-fprintf(1, 'Screen resolution:   %d x %d\n\n', Rect(3), Rect(4));
+    % print header
+    OutText{OutIndex} = sprintf('\n*** GetDevices INFORMATION ***\n');
+    OutIndex = OutIndex + 1;
 
-% devices 
-[indices, names, infos] = GetKeyboardIndices;
+    % get version
+    OutText{OutIndex} = sprintf('%s\n\n', PsychtoolboxVersion);
+    OutIndex = OutIndex + 1;
 
-for i = 1:numel(indices)
-    fprintf('%d:%s\n', indices(i), names{i});
+    % get all screens information
+    for i = ScreenNumber
+        Hz = Screen('FrameRate', i);
+        SRect = Screen('Rect', i);
+
+        OutText{OutIndex} = sprintf('Screen %d, FrameRate:  %0.2f Hz\n', ...
+            i, Hz);
+        OutIndex = OutIndex + 1;
+
+        OutText{OutIndex} = sprintf('Screen %d, Resolution: %d x %d\n', ...
+            i, SRect(3), SRect(4));
+        OutIndex = OutIndex + 1;
+    end
+    clear i;
+    OutText{OutIndex} = sprintf('\n');
+    OutIndex = OutIndex + 1;
+    
+    % get screen information
+    OutText{OutIndex} = [sprintf('Screen refresh rate: %0.4f Hz\n', 1/Refresh) ...
+        sprintf('Screen resolution  : %d x %d\n\n', Rect(3), Rect(4))];
+    OutIndex = OutIndex + 1;
+    
+    % get devices devices 
+    [indices, names, infos] = GetKeyboardIndices;
+    for i = 1:numel(indices)
+        OutText{OutIndex} = sprintf('%d:%s\n', indices(i), names{i});
+        OutIndex = OutIndex + 1;
+    end
+    OutText{OutIndex}  = sprintf('\n');
+    OutIndex = OutIndex + 1;
+    
+    % get graphics card
+    OutText{OutIndex} = sprintf('GLRenderer: %s\n', Info.GLRenderer);
+    OutIndex = OutIndex + 1;
+    OutText{OutIndex} = sprintf('GLVersion : %s\n', Info.GLVersion);
+    OutIndex = OutIndex + 1;
+    
+    % close output
+    OutText{OutIndex} = sprintf('*** GetDevices INFORMATION ***\n\n');
+
+    mkdir(fullfile(pwd, 'ScannerQuery'));
+    OutFile = fullfile(pwd, 'ScannerQuery', 'GetDevices.txt');
+    Fid = fopen(OutFile, 'w');
+    fprintf(1, '\n');
+    for i = 1:numel(OutText)
+        fprintf(1, '%s', OutText{i});
+        fprintf(Fid, '%s', OutText{i});
+    end
+    fprintf(1, '\n');
+    fclose(Fid);
+
+    sca;
+    ShowCursor;
+    ListenChar(0);
+    Priority(0);
+catch
+    ShowCursor;
+    sca;
+    ListenChar(0);
+    psychrethrow(psychlasterror);
+    Priority(0);
 end
 
-% close output
-fprintf(1, '*** GetDevices INFORMATION ***\n\n');
-
-sca
+% Rect
+% FrameRate
