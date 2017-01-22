ea9a937c5a271cd5671c47a1d4e46a56675c9926
Joe Heffernan <heffjos@umich.edu>
2017-01-22 12:49:11 -0600

    Updated design. Changed number of runs from 2 to 4 with 36 trials in each run. For each run the split is like this: (A,C 7 positive 2 baseline) (B,D 5 positive, 4 baseline). Added 2 more jitters between infucion/feedback and their rating periods.

--- DebugScripts/Development/CreateDesign.m
+++ DebugScripts/Development/CreateDesign.m
@@ -1,16 +1,25 @@
 clear all;
 rng(1234567890);
 
-% 18 trials each of 1,2,3,4 (A,B,C,D)
-% 15 1,3 positive
-% 3 1,3 baseline
-% 10 2,4 positive
-% 8 2,4 baseline
+% 4 runs
+%   36 trials per run
+%   9 trials for each condition in 1 run
+%       7 1,3 positive
+%       2 1,3 baseline
+%       5 2,4 positive
+%       4 2,4 baseline
+% 9 trials of each (1, 2, 3, 4)
 
-NumRuns = 2;
-NumTrials = 72;
+NumRuns = 4;
+NumTrials = 36;
 Trials = [1 2 3 4];
 Possible = PrintAllPermutations(Trials);
+NumJitters = 4;
+TrialBlocks = 9;
+UniquePositive = 7;
+UniqueBaseline = 5;
+NumBaseAC = 2;
+NumPosBD = 4;
 
 fid = fopen('Design.csv', 'w');
 fprintf(fid, 'Run,');
@@ -19,60 +28,62 @@ fprintf(fid, 'Infusion,');
 fprintf(fid, 'Feedback,');
 fprintf(fid, 'Waveform,');
 fprintf(fid, 'Jitter1Dur,');
-fprintf(fid, 'Jitter2Dur\n');
+fprintf(fid, 'Jitter2Dur,');
+fprintf(fid, 'Jitter3Dur,');
+fprintf(fid, 'Jitter4Dur\n');
 
 for i = 1:NumRuns
 
     % get trial order
-    Blocks = randperm(size(Possible, 1), 18);
+    Blocks = randperm(size(Possible, 1), TrialBlocks);
     RunTrials = Possible(Blocks, :);
     RunTrials = RunTrials';
     RunTrials = RunTrials(:);
 
-    % make random jitter1 and 2
+    % make 4 exponentially distributed distributions
     lambda = 1/60;
-    c1 = 15;
+    c1 = 5;
     c2 = 120;
     e1 = exp(-lambda*c1);
     e2 = exp(-lambda*c2);
-    Jitter = floor(-1/lambda * log(e1-rand(NumTrials, 2)*(e1-e2)));
+    Jitter = floor(-1/lambda * log(e1-rand(NumTrials, NumJitters)*(e1-e2)));
 
     % assign waveforms
     Waveforms = zeros(NumTrials, 1);
     Feedback = zeros(NumTrials, 1);
 
     ProtALoc = find(RunTrials == 1);
-    ProtAPosLoc = ProtALoc(randperm(length(ProtALoc), 15));
+    ProtAPosLoc = ProtALoc(randperm(length(ProtALoc), UniquePositive));
     ProtABaseLoc = setdiff(ProtALoc, ProtAPosLoc);
-    Waveforms(ProtAPosLoc) = randperm(15);
+    Waveforms(ProtAPosLoc) = randperm(UniquePositive);
     Feedback(ProtAPosLoc) = 1;
-    Waveforms(ProtABaseLoc) = randperm(8, 3);
+    Waveforms(ProtABaseLoc) = randperm(UniqueBaseline, NumBaseAC);
     Feedback(ProtABaseLoc) = 2;
 
     ProtCLoc = find(RunTrials == 3);
-    ProtCPosLoc = ProtCLoc(randperm(length(ProtCLoc), 15));
+    ProtCPosLoc = ProtCLoc(randperm(length(ProtCLoc), UniquePositive));
     ProtCBaseLoc = setdiff(ProtCLoc, ProtCPosLoc);
-    Waveforms(ProtCPosLoc) = randperm(15);
+    Waveforms(ProtCPosLoc) = randperm(UniquePositive);
     Feedback(ProtCPosLoc) = 1;
     UsedBase = Waveforms(ProtABaseLoc);
     Waveforms(ProtCBaseLoc) = UsedBase(randperm(length(UsedBase)));
     Feedback(ProtCBaseLoc) = 2;
 
     ProtBLoc = find(RunTrials == 2);
-    ProtBPosLoc = ProtBLoc(randperm(length(ProtBLoc), 10));
+    ProtBPosLoc = ProtBLoc(randperm(length(ProtBLoc), NumPosBD));
     ProtBBaseLoc = setdiff(ProtBLoc, ProtBPosLoc);
-    Waveforms(ProtBPosLoc) = randperm(15, 10);
+    Waveforms(ProtBPosLoc) = randperm(UniquePositive, NumPosBD);
     Feedback(ProtBPosLoc) = 1;
-    Waveforms(ProtBBaseLoc) = randperm(8);
+    Waveforms(ProtBBaseLoc) = randperm(UniqueBaseline);
     Feedback(ProtBBaseLoc) = 2;
 
     ProtDLoc = find(RunTrials == 4);
-    ProtDPosLoc = ProtDLoc(randperm(length(ProtDLoc), 10));
+    ProtDPosLoc = ProtDLoc(randperm(length(ProtDLoc), NumPosBD));
     ProtDBaseLoc = setdiff(ProtDLoc, ProtDPosLoc);
     UsedPos = Waveforms(ProtBPosLoc);
     Waveforms(ProtDPosLoc) = UsedPos(randperm(length(UsedPos)));
     Feedback(ProtDPosLoc) = 1;
-    Waveforms(ProtDBaseLoc) = randperm(8);
+    Waveforms(ProtDBaseLoc) = randperm(UniqueBaseline);
     Feedback(ProtDBaseLoc) = 2;
 
     for k = 1:NumTrials
@@ -99,7 +110,9 @@ for i = 1:NumRuns
 
         fprintf(fid, '%d,', Waveforms(k));
         fprintf(fid, '%0.4f,', Jitter(k, 1));
-        fprintf(fid, '%0.4f\n', Jitter(k, 2));
+        fprintf(fid, '%0.4f,', Jitter(k, 2));
+        fprintf(fid, '%0.4f,', Jitter(k, 3));
+        fprintf(fid, '%0.4f\n', Jitter(k, 4));
     end
 end
 fclose(fid);
