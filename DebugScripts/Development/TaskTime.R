c34f71b7be0938fe1713d719d202d85017f7fdc6
Joe Heffernan <heffjos@umich.edu>
2017-01-24 13:04:48 -0600

    Updated TaskTime to print out total task length with different input variabls.

--- DebugScripts/Development/TaskTime.R
+++ DebugScripts/Development/TaskTime.R
@@ -5,6 +5,7 @@ MeanJitter <- function(Min, Max, Lambda) {
   term2 <- Min*exp(-Lambda*Min) - Max*exp(-Lambda*Max)
   term3 <- exp(-Lambda*Min) - exp(-Lambda*Max)
   value <- term1*((1/Lambda)*term2 + (1/Lambda^2)*term3)
+  value
 }
 
 BoundedExp <- function(Min, Max, Lambda, N) {
@@ -13,11 +14,29 @@ BoundedExp <- function(Min, Max, Lambda, N) {
   samples <- (-1/Lambda * log(e1 - runif(N) * (e1 - e2)))
 }
 
-# PossibleTimes <- expand.grid(
-#   TotalTrials = c(36*4, 32*4)
-#   MinJitter = c(0, 5, 10, 15, 20, 25)
-#   MaxJitter = c(2)
-#   TrialTime = c(16, 17, 18)
+PossibleTimes <- expand.grid(
+  TotalTrials = c(36*4, 32*4),
+  TrialTime = c(16, 17, 18),
+  MinJitter = c(0, 5, 10, 15, 20, 25)/60,
+  MaxJitter = c(120, 180)/60) %>%
+  arrange(TotalTrials, TrialTime, MinJitter, MaxJitter)
+
+Lambda <- 60
+
+PossibleTimes <- PossibleTimes %>%
+  mutate(JitterSeconds = MeanJitter(MinJitter, MaxJitter, 1) * TotalTrials * 4, 
+    TrialSeconds = TrialTime * TotalTrials,
+    JitterMinutes = JitterSeconds / 60,
+    TrialMinutes = TrialSeconds / 60,
+    TotalSeconds = JitterSeconds + TrialSeconds,
+    TotalMinutes = TotalSeconds / 60)
+
+SelectedTimes <- PossibleTimes %>%
+  filter(TotalMinutes <= 46 & TotalMinutes >= 42) %>%
+  arrange(TotalMinutes)
+
+write.csv(SelectedTimes, file="PossibleTimes.csv", quote=F, row.names=F)
+  
 
 
 BaseTrialDur <- 16
@@ -25,7 +44,7 @@ data <- read.csv("Design.csv")
 MeanJitter <- mean(c(data$Jitter1Dur, data$Jitter2Dur, data$Jitter3Dur, data$Jitter4Dur))
 data.time <- data %>%
   group_by(Run) %>%
-  summarize(Seconds = (sum(Jitter1Dur) + sum(Jitter2Dur) + sum(Jitter3Dur) + sum(Jitter4Dur))/60 + 18*n(),
+  summarize(Seconds = (sum(Jitter1Dur) + sum(Jitter2Dur) + sum(Jitter3Dur) + sum(Jitter4Dur))/60 + BaseTrialDur*n(),
     Minutes = Seconds/60)
 TotalMinutes <- sum(data.time$Minutes)
     
